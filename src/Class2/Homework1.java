3e00272d5fb4fe9edfd735a7b3f4dcb156edc2b0
Lolieana Botros <lolieanabotros@lolieanas-air.mynetworksettings.com>
2022-03-05 13:07:17 -0500

    Homework_1

---
+++ src/Class2/Homework1.java
@@ -0,0 +1,63 @@
+package Class2;
+
+public class Homework1 {
+
+    public static void main(String[] args) {
+
+        // Store values for below scenario in an appropriate variable, and print the same in Console.
+        // 1. number of hours in a day
+        byte numberOfHoursInDay = 24;
+        System.out.println("Number of hours in a day -> " + numberOfHoursInDay);
+
+
+        // 2. max no. of days in an year
+        short maxNumOfDaysInYear = 365;
+        System.out.println("Number of days in an year = " + maxNumOfDaysInYear);
+
+
+        // 3. total number of employees in an organization
+        int numOfEmployeeInOrganization = 600;
+        System.out.println("Number of employees in an organization = " +  numOfEmployeeInOrganization);
+
+
+        // 4. Population in a country
+        long populationInCountry = 1098055L;
+        System.out.println("Number of population in a country = " + populationInCountry);
+
+
+        // 5. Mortgage rate
+        float mortgageRate = 2.3f;
+        System.out.println("Mortgage rate is : " + mortgageRate);
+
+
+        // 6. Balance in an bank account
+        double accountBalance = 15345.30;
+        System.out.println("Your account balance = " + accountBalance);
+
+
+        // 7. does the sun rise from the West?
+        boolean doesTheSunRiseFromTheWest = false;
+        System.out.println("Does the sun rise from west? " + doesTheSunRiseFromTheWest);
+
+        // 8. Initials of your name (Use: char)
+        char firstInitial = 'L';
+        char secondInitial = 'B';
+        System.out.println("My initials are : " + firstInitial + secondInitial);
+
+        // 9. Store your full name (Use: String)
+        String myName = "Lolieana Botros";
+        System.out.println("My full name is : " + myName);
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
